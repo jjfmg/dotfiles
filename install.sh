8ccc6164affdd605155a0857dfe387f7500b5271
Joshua Gonzales <j@joshua.bio>
2015-07-28 18:14:53 -0700

    forgot prezto

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
-
